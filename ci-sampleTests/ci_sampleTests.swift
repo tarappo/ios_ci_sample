995d24f74cdab8d0b5c449eaa2f2a1d18acb0ea1
tarappo <thirata@pplace.jp>
2019-03-03 05:38:31 +0900

    add unit test target

---
+++ ci-sampleTests/ci_sampleTests.swift
@@ -0,0 +1,32 @@
+//
+//  ci_sampleTests.swift
+//  ci-sampleTests
+//
+//  Created by Toshiyuki Hirata on 2019/03/03.
+//  Copyright © 2019 tarappo. All rights reserved.
+//
+
+import XCTest
+
+class ci_sampleTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        XCTAssert(true)
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
