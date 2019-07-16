1ddcc955d54e6c958a39a34458d61034230c084c
tarappo <thirata@pplace.jp>
2019-07-17 08:41:04 +0900

    add unit test 1case

--- ci-sampleTests/ci_sampleTests.swift
+++ ci-sampleTests/ci_sampleTests.swift
@@ -7,26 +7,19 @@
 //
 
 import XCTest
+@testable import ci_sample
 
 class ci_sampleTests: XCTestCase {
 
     override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
     }
 
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() {
-        XCTAssert(true)
+    func testCalc() {
+        let sample = Sample()
+        let result = sample.add(3, 4)
+        XCTAssertEqual(result, 7)
     }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
