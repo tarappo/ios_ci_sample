d89e60be7eefb523146e5cb1177e5b4a9c8ebaef
tarappo <tarappo@gmail.com>
2019-08-23 23:37:48 +0900

    update method for test

---
+++ ci-sample/ForTest/Sample.swift
@@ -0,0 +1,19 @@
+//
+//  Sample.swift
+//  ci-sample
+//
+//  Created by Toshiyuki Hirata on 2019/07/17.
+//  Copyright © 2019 tarappo. All rights reserved.
+//
+
+import Foundation
+
+class Sample {
+    func add(_ n1: Int, _ n2: Int) -> Int {
+        return n1 + n2
+    }
+
+    func multiplication(_ n1: Int, _ n2: Int) -> Int {
+        return n1 * n2
+    }
+}
